35bdfe6ee23ce013392a6b31aa3e0ed718a8f728
foultrip <davidvasquezmahecha@gmail.com>
2024-01-13 16:40:58 -0500

    correccion de bug

--- src/app/api/stream/route.ts
+++ src/app/api/stream/route.ts
@@ -14,13 +14,13 @@ export async function POST() {
     new_asset_settings: { playback_policy: "public" },
   });
 
-  console.log(response)
+  console.log(response);
 
   const datesStream = {
     live_stream_id: response.id,
     stream_key: response.stream_key,
-    playbackId: response.playback_ids[0].id,
-  }
+    playbackId: response.playback_ids ? response.playback_ids[0].id : undefined,
+  };
 
   return NextResponse.json(datesStream);
 }
